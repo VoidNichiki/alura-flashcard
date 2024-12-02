6c40ac2d41ed253e2d7d2b4ee2ee176c47968f05
Gabriel Canudo <zacknichiki@gmail.com>
2024-12-02 02:07:21 -0300

    firsts archives

---
+++ Super trunfo/perguntas.js
@@ -0,0 +1,82 @@
+criaCartao(
+    'Animes',
+    'Como se chama o protagonista de Naruto?',
+    'Naruto'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Austria?',
+    'Viena'
+)
+
+criaCartao(
+    'RPG',
+    'Qual a diferenca entre um personagem e um NPC?',
+    'Um personagem tem vida, um NPC nao'
+)
+
+criaCartao(
+    'Matematica',
+    'Qual o resultado de 2+2?',
+    '4'
+)
+criaCartao(
+    'Animes',
+    'Qual o nome do protagonista de Dragon Ball?',
+    'Goku'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'Brasilia'
+)
+
+criaCartao(
+    'Jogo',
+    'Minecraft foi criado por... ',
+    'Notch'
+)
+
+criaCartao(
+    'Matematica',
+    'Qual o resultado de 1+1?',
+    '2'
+)
+
+criaCartao(
+    'Animes',
+    'Qual o nome do protagonista de One Piece?',
+    'Monkey D. Luffy'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Peru?',
+    'Lima'
+)
+
+criaCartao(
+    'Jogos',
+    'O jogo que roda em qualquer lugar se chama...',
+    'Doom'
+)
+
+criaCartao(
+    'Matematica',
+    'Qual o resultado de 2+3?',
+    '5'
+)
+
+criaCartao(
+    'Animes',
+    'Qual o nome do protagonista de Bleach?',
+    'Ichigo Kurosaki'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Franca?',
+    'Paris'
+)
